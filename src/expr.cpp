89afd0f22c4799f33011065dd5e2c82f82e9383e
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2023-10-31 08:31:11 -0700

    Allow more cases of enum values broadcasting to varying

--- src/expr.cpp
+++ src/expr.cpp
@@ -6895,7 +6895,9 @@ static llvm::Value *lUniformValueToVarying(FunctionEmitContext *ctx, llvm::Value
                                               CastType<AtomicType>(type), pos);
     }
 
-    Assert(CastType<PointerType>(type) != nullptr);
+    // EnumType is almost like atomic and may be should be treated like one,
+    // but SmearUniform also works well with broadcasting it to varying type.
+    Assert(CastType<PointerType>(type) != nullptr || CastType<EnumType>(type) != nullptr);
     return ctx->SmearUniform(value);
 }
 
