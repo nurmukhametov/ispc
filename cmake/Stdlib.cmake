f4698c9610085982e7ed0411bbf40a6aaeec0021
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-07-16 15:31:51 +0100

    Fix stdlib CMake

--- cmake/Stdlib.cmake
+++ cmake/Stdlib.cmake
@@ -6,28 +6,156 @@
 #
 # ispc Stdlib.cmake
 #
-function(create_stdlib mask outputPath)
-    set(output ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/stdlib_mask${mask}_ispc.cpp)
-    add_custom_command(
-        OUTPUT ${output}
-        COMMAND ${CLANG_EXECUTABLE} -E -x c -DISPC_MASK_BITS=${mask} -DISPC=1 -DPI=3.14159265358979
-            stdlib.ispc | \"${Python3_EXECUTABLE}\" stdlib2cpp.py mask${mask}
-            > ${output}
-        DEPENDS stdlib.ispc
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-    set(${outputPath} ${output} PARENT_SCOPE)
-    set_source_files_properties(${outputPath} PROPERTIES GENERATED true)
+
+set(STDLIB_ISPC_FILES stdlib.ispc builtins.isph stdlib.isph svml.isph target.isph )
+
+function (write_stdlib_bitcode_lib name target os bit)
+    set(arch "error")
+    if ("${target}" MATCHES "sse|avx")
+        if ("${bit}" STREQUAL "32")
+            set(arch "x86")
+        elseif ("${bit}" STREQUAL "64")
+            set(arch "x86_64")
+        else()
+            set(arch "error")
+        endif()
+    elseif ("${target}" MATCHES "neon")
+        if ("${bit}" STREQUAL "32")
+            set(arch "arm")
+        elseif ("${bit}" STREQUAL "64")
+            set(arch "aarch64")
+        else()
+            set(arch "error")
+        endif()
+    elseif ("${target}" MATCHES "wasm")
+        if ("${bit}" STREQUAL "32")
+            set(arch "wasm32")
+        elseif ("${bit}" STREQUAL "64")
+            set(arch "wasm64")
+        else()
+            set(arch "error")
+        endif()
+    elseif ("${target}" MATCHES "gen9|xe")
+        set(arch "xe64")
+    endif()
+    if ("${arch}" STREQUAL "error")
+        message(FATAL_ERROR "Incorrect target or bit passed to write_stdlib_bitcode_lib ${target} ${os} ${bit}")
+    endif()
+
+    if ("${os}" STREQUAL "unix")
+        set(os "linux")
+    endif()
+
+    string(REPLACE "-" "_" target ${target})
+    file(APPEND ${CMAKE_BINARY_DIR}/bitcode_libs_generated.cpp
+      "static BitcodeLib ${name}(\"${name}.bc\", ISPCTarget::${target}, TargetOS::${os}, Arch::${arch});\n")
+
+    set(canon_os ${os} PARENT_SCOPE)
 endfunction()
 
-function(generate_stdlib resultList)
-    foreach (m ${ARGN})
-        create_stdlib(${m} outputPath)
-        list(APPEND tmpList "${outputPath}")
-        if(MSVC)
-            # Group generated files inside Visual Studio
-            source_group("Generated Stdlib" FILES ${outputPath})
-        endif()
-    endforeach()
-    set(${resultList} ${tmpList} PARENT_SCOPE)
+function (stdlib_to_cpp target bit os)
+    set(name stdlib-${target}-${bit}bit-${os})
+    string(REPLACE "-" "_" name "${name}")
+    set(cpp ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${name}.cpp)
+    set(bc ${BITCODE_FOLDER}/${name}.bc)
+
+    # define canon_os
+    write_stdlib_bitcode_lib(${name} ${target} ${arch} ${os})
+
+    add_custom_command(
+        OUTPUT ${bc}
+        COMMAND ${PROJECT_NAME} --nostdlib --gen-stdlib --target=${target} --arch=${arch} --target-os=${canon_os} stdlib.ispc --emit-llvm -o ${bc}
+        DEPENDS ${PROJECT_NAME} ${STDLIB_ISPC_FILES}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+
+    string(TOUPPER ${os} OS_UP)
+    add_custom_command(
+        OUTPUT ${cpp}
+        COMMAND ${Python3_EXECUTABLE} bitcode2cpp.py ${bc} --type=stdlib --runtime=${bit} --os=${OS_UP} ${cpp}
+        DEPENDS bitcode2cpp.py ${bc}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+
+    set(tmp_list_bc ${STDLIB_BC_FILES})
+    list(APPEND tmp_list_bc ${bc})
+    set(STDLIB_BC_FILES ${tmp_list_bc} PARENT_SCOPE)
+
+    set(tmp_list_cpp ${STDLIB_CPP_FILES})
+    list(APPEND tmp_list_cpp ${cpp})
+    set(STDLIB_CPP_FILES ${tmp_list_cpp} PARENT_SCOPE)
+endfunction()
+
+function (generate_stdlibs_1)
+    list(APPEND os_list)
+    if (ISPC_WINDOWS_TARGET)
+        list(APPEND os_list "windows")
+    endif()
+    if (ISPC_UNIX_TARGET)
+        list(APPEND os_list "unix")
+    endif()
+
+    if (NOT os_list)
+        message(FATAL_ERROR "Windows or Linux target has to be enabled")
+    endif()
+
+    # "Regular" targets, targeting specific real ISA: sse/avx
+    if (X86_ENABLED)
+        foreach (target ${X86_TARGETS})
+            foreach (bit 32 64)
+                foreach (os ${os_list})
+                    stdlib_to_cpp(${target} ${bit} ${os})
+                endforeach()
+            endforeach()
+        endforeach()
+    endif()
+
+    # XE targets
+    if (XE_ENABLED)
+        foreach (target ${XE_TARGETS})
+            foreach (os ${os_list})
+                stdlib_to_cpp(${target} 64 ${os})
+            endforeach()
+        endforeach()
+    endif()
+
+    # ARM targets
+    if (ARM_ENABLED)
+        foreach (os ${os_list})
+            foreach (target ${ARM_TARGETS})
+                stdlib_to_cpp(${target} 32 ${os})
+            endforeach()
+            # Not all targets have 64bit
+            stdlib_to_cpp(neon-i32x4 64 ${os})
+            stdlib_to_cpp(neon-i32x8 64 ${os})
+        endforeach()
+    endif()
+
+    # WASM targets
+    if (WASM_ENABLED)
+        foreach (target ${WASM_TARGETS})
+            foreach (bit 32 64)
+                stdlib_to_cpp(${target} ${bit} web)
+            endforeach()
+        endforeach()
+    endif()
+endfunction()
+
+function (generate_stdlibs)
+    file(WRITE ${CMAKE_BINARY_DIR}/bitcode_libs_generated.cpp)
+
+    add_custom_target(stdlibs-bc)
+    add_custom_target(stdlibs-cpp)
+
+    generate_stdlibs_1()
+
+    if (MSVC)
+        source_group("Generated Stdlib Files" FILES ${STDLIB_CPP_FILES})
+    endif()
+    set_source_files_properties(${STDLIB_CPP_FILES} PROPERTIES GENERATED true)
+
+    set(STDLIB_CPP_FILES ${STDLIB_CPP_FILES} PARENT_SCOPE)
+
+    add_library(stdlib OBJECT EXCLUDE_FROM_ALL ${STDLIB_CPP_FILES})
+    add_dependencies(stdlib stdlibs-cpp)
 endfunction()
