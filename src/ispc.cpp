72a6128e123c6217d6c58af64b464117224637af
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-07-18 13:23:46 +0100

    src/ispc.[h|cpp]

--- src/ispc.cpp
+++ src/ispc.cpp
@@ -2310,6 +2310,7 @@ Globals::Globals() {
     debugPMTimeTrace = false;
     astDump = Globals::ASTDumpKind::None;
     dumpFile = false;
+    genStdlib = false;
     printTarget = false;
     NoOmitFramePointer = false;
     debugIR = -1;
