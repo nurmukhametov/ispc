011768009c200f14b12f538e11f0a84f974f4740
Aleksei Nurmukhametov <aleksei.nurmukhametov@intel.com>
2024-09-19 14:18:53 +0100

    expr.cpp: refactor gotos to fix clang-tidy warning

--- src/expr.cpp
+++ src/expr.cpp
@@ -8897,6 +8897,15 @@ int FunctionSymbolExpr::computeOverloadCost(const FunctionType *ftype, const std
     return costSum;
 }
 
+static bool lReportErrorUnableToFindOverload(std::vector<Symbol *> &matches, const std::vector<const Type *> &argTypes,
+                                             const std::vector<bool> *argCouldBeNULL, std::string &name,
+                                             SourcePos pos) {
+    std::string candidateMessage = lGetOverloadCandidateMessage(matches, argTypes, argCouldBeNULL);
+    Error(pos, "Unable to find any matching overload for call to function \"%s\".\n%s", name.c_str(),
+          candidateMessage.c_str());
+    return false;
+}
+
 bool FunctionSymbolExpr::ResolveOverloads(SourcePos argPos, const std::vector<const Type *> &argTypes,
                                           const std::vector<bool> *argCouldBeNULL,
                                           const std::vector<bool> *argIsConstant) {
@@ -8932,7 +8941,7 @@ bool FunctionSymbolExpr::ResolveOverloads(SourcePos argPos, const std::vector<co
     std::vector<int *> candidateExpandCosts;
 
     if (actualCandidates.size() == 0)
-        goto failure;
+        return lReportErrorUnableToFindOverload(matches, argTypes, argCouldBeNULL, name, pos);
 
     // Compute the cost for calling each of the candidate functions
     for (int i = 0; i < (int)actualCandidates.size(); ++i) {
@@ -8951,7 +8960,8 @@ bool FunctionSymbolExpr::ResolveOverloads(SourcePos argPos, const std::vector<co
     }
     // None of the candidates matched
     if (bestMatchCost == (1 << 30))
-        goto failure;
+        return lReportErrorUnableToFindOverload(matches, argTypes, argCouldBeNULL, name, pos);
+
     for (int i = 0; i < (int)candidateCosts.size(); ++i) {
         if (candidateCosts[i] == bestMatchCost) {
             for (int j = 0; j < (int)candidateCosts.size(); ++j) {
@@ -8989,11 +8999,7 @@ bool FunctionSymbolExpr::ResolveOverloads(SourcePos argPos, const std::vector<co
         return false;
     } else {
         // No matches at all
-    failure:
-        std::string candidateMessage = lGetOverloadCandidateMessage(matches, argTypes, argCouldBeNULL);
-        Error(pos, "Unable to find any matching overload for call to function \"%s\".\n%s", name.c_str(),
-              candidateMessage.c_str());
-        return false;
+        return lReportErrorUnableToFindOverload(matches, argTypes, argCouldBeNULL, name, pos);
     }
 }
 
